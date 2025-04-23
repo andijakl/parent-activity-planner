9dc3c1c59fb9bbe5acebe7ef517eb3d04a414b30
andijakl <andreas.jakl@live.com>
2025-04-23 18:56:12 +0200

    Fix issues and disable minification for debugging

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: false, // Disable minification
+    sourcemap: true, // Generate source maps for easier debugging
+  }
 })
